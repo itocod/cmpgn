cc855b8191c9a4e5af1c0e86369a2a1cce26794c
KISRO-cloudS <kcollino39@gmail.com>
2024-07-11 21:41:19 +0300

    Added render.yaml for deployment configuration

--- buskx/settings.py
+++ buskx/settings.py
@@ -88,18 +88,11 @@ WSGI_APPLICATION = 'buskx.wsgi.application'
 
 
 # Configure databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Update database configuration with $DATABASE_URL.
-DATABASE_URL = os.getenv('DATABASE_URL')
-if DATABASE_URL:
-    db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-    DATABASES['default'].update(db_from_env)
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
