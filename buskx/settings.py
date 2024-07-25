c601b5d774811dc4925d44e21b24228d4bc68278
KISRO-cloudS <kcollino39@gmail.com>
2024-07-25 14:03:47 +0300

    Configured media files handling

--- buskx/settings.py
+++ buskx/settings.py
@@ -241,3 +241,7 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB (increase as needed)
 # Redirect URLs
 LOGIN_REDIRECT_URL = '/rallynex-logo/'
 LOGOUT_REDIRECT_URL = 'index'
+
+# Media files settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
