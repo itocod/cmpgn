197f831e29f023696fc81517e3e07017cebad3b9
KISRO-cloudS <kcollino39@gmail.com>
2024-08-18 14:38:31 +0300

    Your commit message

--- buskx/settings.py
+++ buskx/settings.py
@@ -21,7 +21,7 @@ load_dotenv(env_file)
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'False'
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
