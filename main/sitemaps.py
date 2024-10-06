43f70ab81c40786e67c4758831f470dd79e35bf9
KISRO-cloudS <kcollino39@gmail.com>
2024-10-06 19:26:33 +0300

    Initial commit

--- main/sitemaps.py
+++ main/sitemaps.py
@@ -3,16 +3,17 @@ from django.urls import reverse
 from django.conf import settings
 from .models import Campaign  # Make sure Campaign model is imported
 
+
 class StaticViewSitemap(Sitemap):
     priority = 0.5
     changefreq = 'daily'
 
     def items(self):
-        return ['index', 'privacy_policy', 'terms_of_service', 'project_support']  # Static view names
+        return ['index', 'privacy_policy', 'terms_of_service', 'project_support']
 
     def location(self, item):
-        # Manually append the domain for absolute URL generation
-        return settings.SITE_URL + reverse(item)
+        # Use 'https://' in SITE_URL
+        return f"{settings.SITE_URL}{reverse(item)}"
 
 class CampaignSitemap(Sitemap):
     changefreq = 'weekly'
@@ -25,5 +26,5 @@ class CampaignSitemap(Sitemap):
         return obj.timestamp
 
     def location(self, obj):
-        # Manually append the domain for absolute URL generation
-        return settings.SITE_URL + reverse('view_campaign', args=[obj.id])
+        # Ensure that the URL is correctly prefixed with the site domain
+        return f"{settings.SITE_URL}{reverse('view_campaign', args=[obj.id])}"
