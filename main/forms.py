3c35fa5934c45839e623b8ebe747b0c94167e6a4
KISRO-cloudS <kcollino39@gmail.com>
2025-01-04 16:28:00 +0300

    Fix image orientation on upload

--- main/forms.py
+++ main/forms.py
@@ -166,7 +166,11 @@ class UserForm(forms.ModelForm):
         }
 
 
+
 from PIL import Image, ExifTags
+from io import BytesIO
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 class ProfileForm(forms.ModelForm):
     class Meta:
@@ -189,25 +193,34 @@ class ProfileForm(forms.ModelForm):
             try:
                 image = Image.open(instance.image)
                 for orientation in ExifTags.TAGS.keys():
-                    if ExifTags.TAGS[orientation]=='Orientation':
+                    if ExifTags.TAGS[orientation] == 'Orientation':
                         break
-                exif=dict(image._getexif())
-                if exif[orientation] == 3:
+                exif = dict(image._getexif())
+                if exif.get(orientation) == 3:
                     image = image.rotate(180, expand=True)
-                elif exif[orientation] == 6:
+                elif exif.get(orientation) == 6:
                     image = image.rotate(270, expand=True)
-                elif exif[orientation] == 8:
+                elif exif.get(orientation) == 8:
                     image = image.rotate(90, expand=True)
             except (AttributeError, KeyError, IndexError):
                 # cases: image doesn't have EXIF data or is not a valid image
                 pass
 
-            # Save the corrected image
-            image.save(instance.image.path)
+            # Save the corrected image to S3
+            image_io = BytesIO()
+            image.save(image_io, format='JPEG')  # Use the appropriate format (e.g., PNG, JPEG)
+            image_io.seek(0)
+
+            # Get the image name and upload to S3
+            file_name = instance.image.name
+            instance.image.save(file_name, ContentFile(image_io.read()), save=False)
 
         if commit:
             instance.save()
         return instance
+
+
+
 
 
 
